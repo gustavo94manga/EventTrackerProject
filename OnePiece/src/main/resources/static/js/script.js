74be595efcc460ba097fed56b8a6937cd7b2a947
Angel Garza <gustavo94manga@gmail.com>
2023-04-09 04:45:43 +0900

    implement Delete function method for JavaScript

--- OnePiece/src/main/resources/static/js/script.js
+++ OnePiece/src/main/resources/static/js/script.js
@@ -40,165 +40,190 @@ function init() {
 			age: form.age.value
 		};
 
-		let charaId = form.charaId.value
-		
-			console.log(charaId + updateCharacter);
-			updateACharacter(updateCharacter, charaId);
-		});
+		let charaId = form.charaId.value;
 
-	}
+		console.log(charaId + updateCharacter);
+		updateACharacter(updateCharacter, charaId);
+	});
+	
+	//Delete Character
+	document.deleteCharacterForm.deleteCharacter.addEventListener('click', function(e){
+		e.preventDefault();
+		let form = document.deleteCharacterForm;
+		let charaId = form.charaId.value;
+		deleteACharacter(charaId);
+	});
+
+}
 
 function getSingleCharacter(charaId) {
-			let xhr = new XMLHttpRequest();
-			xhr.open('GET', 'api/characters/' + charaId);
-			xhr.onreadystatechange = function() {
-				if (xhr.readyState === 4) {
-					if (xhr.status === 200) {
-						let characterJson = xhr.responseText;
-						let singleCharacter = JSON.parse(characterJson);
-						displaySingleCharacter(singleCharacter);
-						//loadAllCharacters(charaId);
-					}
-					else {
-						console.error('Request failed: ' + xhr.status);
-						displayError('Film not found: ' + charaId);
-					}
-				}
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', 'api/characters/' + charaId);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
+				let characterJson = xhr.responseText;
+				let singleCharacter = JSON.parse(characterJson);
+				displaySingleCharacter(singleCharacter);
+				//loadAllCharacters(charaId);
 			}
-			xhr.send();
+			else {
+				console.error('Request failed: ' + xhr.status);
+				displayError('Film not found: ' + charaId);
+			}
 		}
+	}
+	xhr.send();
+}
 
 function displaySingleCharacter(singleCharacter) {
-			let dataDiv = document.getElementById('characterData');
-			dataDiv.textContent = '';
-			// TODO:
-			// * Create and append elements to the data div to display:
-			// * Film title (h1) and description (blockquote).
-			let h2 = document.createElement('h2');
-			let h3 = document.createElement('h2');
-			let ul = document.createElement('ul');
-			let li = document.createElement('li');
-			h2.textContent = singleCharacter.firstName;
-			h3.textContent = singleCharacter.lastName;
+	let dataDiv = document.getElementById('characterData');
+	dataDiv.textContent = '';
+	// TODO:
+	// * Create and append elements to the data div to display:
+	// * Film title (h1) and description (blockquote).
+	let h2 = document.createElement('h2');
+	let h3 = document.createElement('h2');
+	let ul = document.createElement('ul');
+	let li = document.createElement('li');
+	h2.textContent = singleCharacter.firstName;
+	h3.textContent = singleCharacter.lastName;
 
-			li.textContent = singleCharacter.age;
-			ul.appendChild(li);
+	li.textContent = singleCharacter.age;
+	ul.appendChild(li);
 
 
-			dataDiv.appendChild(h2);
-			dataDiv.appendChild(h3);
-			dataDiv.appendChild(ul);
-			// * Rating, release year, and length as an unordered list.
-			//If the new film created also had actors, I could call the getFilmActors from here 
-			//instead of on getFilm and
-			//it would display the actors and the new film created as well.
-			// getFilmActors(filmId);
-		}
+	dataDiv.appendChild(h2);
+	dataDiv.appendChild(h3);
+	dataDiv.appendChild(ul);
+	// * Rating, release year, and length as an unordered list.
+	//If the new film created also had actors, I could call the getFilmActors from here 
+	//instead of on getFilm and
+	//it would display the actors and the new film created as well.
+	// getFilmActors(filmId);
+}
 
 function loadAllCharacters() {
-			//XHR stuff
-			let xhr = new XMLHttpRequest();
-			xhr.open('GET', 'api/characters');
-			xhr.onreadystatechange = function() {
-				if (xhr.readyState === 4) {
-					if (xhr.status === 200) {
-						let characterList = JSON.parse(xhr.responseText);
-						displayCharacterList(characterList);
-					}
-				}
-			};
-
-			xhr.send();
+	//XHR stuff
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', 'api/characters');
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
+				let characterList = JSON.parse(xhr.responseText);
+				displayCharacterList(characterList);
+			}
 		}
+	};
+
+	xhr.send();
+}
 // Have an else {} in the crud methods for status === code!!!!!!!!!
 
 function displayCharacterList(characterList) {
-			//DOM stuff
-			let tbody = document.getElementById('characterListBody');
-			tbody.textContent = '';
-			for (let chara of characterList) {
+	//DOM stuff
+	let tbody = document.getElementById('characterListBody');
+	tbody.textContent = '';
+	for (let chara of characterList) {
 
-				let tr = document.createElement('tr');
-				tbody.appendChild(tr);
+		let tr = document.createElement('tr');
+		tbody.appendChild(tr);
 
-				tr.addEventListener('click', function(e) {
-					e.preventDefault();
-					let characterId = e.target.parentElement.firstElementChild.textContent;
-					console.log(characterId);
-					getSingleCharacter(characterId);
-				});
+		tr.addEventListener('click', function(e) {
+			e.preventDefault();
+			let characterId = e.target.parentElement.firstElementChild.textContent;
+			console.log(characterId);
+			getSingleCharacter(characterId);
+		});
 
-				let td = document.createElement('td');
-				td.textContent = chara.id;
-				tr.appendChild(td);
+		let td = document.createElement('td');
+		td.textContent = chara.id;
+		tr.appendChild(td);
 
-				td = document.createElement('td');
-				td.textContent = chara.firstName;
-				tr.appendChild(td);
+		td = document.createElement('td');
+		td.textContent = chara.firstName;
+		tr.appendChild(td);
 
-				let td2 = document.createElement('td');
-				td2.textContent = chara.lastName;
-				tr.appendChild(td2);
+		let td2 = document.createElement('td');
+		td2.textContent = chara.lastName;
+		tr.appendChild(td2);
 
-				let td3 = document.createElement('td');
-				td3.textContent = chara.age;
-				tr.appendChild(td3);
+		let td3 = document.createElement('td');
+		td3.textContent = chara.age;
+		tr.appendChild(td3);
 
-				let td4 = document.createElement('td');
-				td4.textContent = chara.imageUrl;
-				tr.appendChild(td4);
-			}
-		}
+		let td4 = document.createElement('td');
+		td4.textContent = chara.imageUrl;
+		tr.appendChild(td4);
+	}
+}
 
 function displayError(errorMsg) {
-			let characterListDiv = document.getElementById('characterListDiv');
-			characterListDiv.textContent = errorMsg;
-			let dataDiv = document.getElementsByTagName('characterData');
-			dataDiv.textContent = '';
-		}
+	let characterListDiv = document.getElementById('characterListDiv');
+	characterListDiv.textContent = errorMsg;
+	let dataDiv = document.getElementsByTagName('characterData');
+	dataDiv.textContent = '';
+}
 
 function addNewCharacter(newCharacter) {
-			let xhr = new XMLHttpRequest();
-			xhr.open('POST', 'api/characters');
-			xhr.onreadystatechange = function() {
-				if (xhr.readyState === 4) {
-					if (xhr.status === 200 || xhr.status === 201) {
-						let chara = JSON.parse(xhr.responseText);
-						loadAllCharacters();
-						/*displaySingleCharacter(chara);*/
-					}
-					else {
-						console.error('POST Request failed: ' + xhr.status);
-						displayError('Character not created: ' + xhr.status + " : " + xhr.responseText);
-					}
-				}
+	let xhr = new XMLHttpRequest();
+	xhr.open('POST', 'api/characters');
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200 || xhr.status === 201) {
+				let chara = JSON.parse(xhr.responseText);
+				loadAllCharacters();
+				/*displaySingleCharacter(chara);*/
 			}
-			xhr.setRequestHeader("Content-type", "application/json");
-			let newCharacterJson = JSON.stringify(newCharacter);
-			xhr.send(newCharacterJson);
+			else {
+				console.error('POST Request failed: ' + xhr.status);
+				displayError('Character not created: ' + xhr.status + " : " + xhr.responseText);
+			}
 		}
+	}
+	xhr.setRequestHeader("Content-type", "application/json");
+	let newCharacterJson = JSON.stringify(newCharacter);
+	xhr.send(newCharacterJson);
+}
 
 function updateACharacter(updateCharacter, charaId) {
-			let xhr = new XMLHttpRequest();
-			xhr.open('PUT', 'api/characters/' + charaId);
-			xhr.onreadystatechange = function() {
-				if (xhr.readyState === 4) {
-					if (xhr.status === 200 || xhr.status === 201) {
-						let chara = JSON.parse(xhr.responseText);
-						console.log(chara);
-						/*loadAllCharacters();*/
-						displaySingleCharacter(chara);
-					}
-					else {
-						console.error('PUT Request failed: ' + xhr.status);
-						displayError('Character not updated: ' + xhr.status + " : " + xhr.responseText);
-					}
-				}
+	let xhr = new XMLHttpRequest();
+	xhr.open('PUT', 'api/characters/' + charaId);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200 || xhr.status === 201) {
+				let chara = JSON.parse(xhr.responseText);
+				console.log(chara);
+				/*loadAllCharacters();*/
+				displaySingleCharacter(chara);
 			}
-			xhr.setRequestHeader("Content-type", "application/json");
-			let updateCharacterJson = JSON.stringify(updateCharacter);
-			xhr.send(updateCharacterJson);
+			else {
+				console.error('PUT Request failed: ' + xhr.status);
+				displayError('Character not updated: ' + xhr.status + " : " + xhr.responseText);
+			}
 		}
+	}
+	xhr.setRequestHeader("Content-type", "application/json");
+	let updateCharacterJson = JSON.stringify(updateCharacter);
+	xhr.send(updateCharacterJson);
+}
+
+function deleteACharacter(charaId) {
+	let xhr = new XMLHttpRequest();
+	/*xhr.open('DELETE', `characters/${charaId}`, true);*/
+	xhr.open('DELETE', 'api/characters/' + charaId, true);
+	xhr.onreadystatechange = function() {
+		if (xhr.status === 204 && xhr.readyState === 4) {
+				/*loadAllCharacters();*/
+				console.log("Delete Successful");
+		} else {
+			console.log('Delete Unsuccessful');
+		}
+	};
+	xhr.send();
+}
+
+
 
 
 
